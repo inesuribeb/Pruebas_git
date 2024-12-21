77d26b47201fc8720c1b9c1ecebcca165a06a65e
inesuribeb <inesuribeb@gmail.com>
2024-12-21 17:57:52 +0100

    dinamic navbar por mobile phone

--- src/RootPhone.jsx
+++ src/RootPhone.jsx
@@ -1,33 +1,64 @@
 import { Outlet, Link } from 'react-router-dom';
 import './RootPhone.css'
 import DragHandleIcon from "@mui/icons-material/DragHandle";
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
+
 
 function RootPhone() {
- 
+  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleNavClick = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <>
-      
       <header className="header-mobile">
         <h1>SWIPE AGENCY</h1>
-        <nav className="nav-mobile">
-          <ul className="nav-list-mobile">
-            <li className="nav-item-mobile"><Link to="/sanders">Sanders</Link></li>
-            <li className="nav-item-mobile"><Link to="/gloverall">Gloverall</Link></li>
-            <li className="nav-item-mobile"><Link to="/moonstar_jp">Moonstar</Link></li>
-            <li className="nav-item-mobile"><Link to="/ilBussetto">Il Bussetto</Link></li>
-            <li className="nav-item-mobile"><Link to="/moonstar_810s">Moonstar 810s</Link></li>
-            <li className="nav-item-mobile"><Link to="/shoesLikePottery">Shoes Like Pottery</Link></li>
-          </ul>
-        </nav>
+        <div className="menu-icon" onClick={toggleMenu}>
+    
+          {isMenuOpen ? (
+            <CloseIcon
+              sx={{ fontSize: 24, color: 'black' }}
+              onClick={() => setIsMenuOpen(false)}
+            />
+          ) : (
+            <DragHandleIcon
+              sx={{ fontSize: 24, color: 'black' }}
+              onClick={() => setIsMenuOpen(true)}
+            />
+          )}
+        </div>
+
+        {isMenuOpen && (
+
+          <nav className={`nav-mobile ${isMenuOpen ? 'open' : ''}`}>
+            <ul className="nav-list-mobile">
+              <li className="nav-item-mobile"><Link onClick={handleNavClick} to="/">Swipe</Link></li>
+              <li className="nav-item-mobile"><Link onClick={handleNavClick} to="/sanders">Sanders</Link></li>
+              <li className="nav-item-mobile"><Link onClick={handleNavClick} to="/gloverall">Gloverall</Link></li>
+              <li className="nav-item-mobile"><Link onClick={handleNavClick} to="/moonstar_jp">Moonstar</Link></li>
+              <li className="nav-item-mobile"><Link onClick={handleNavClick} to="/ilBussetto">Il Bussetto</Link></li>
+              <li className="nav-item-mobile"><Link onClick={handleNavClick} to="/moonstar_810s">Moonstar 810s</Link></li>
+              <li className="nav-item-mobile"><Link onClick={handleNavClick} to="/shoesLikePottery">Shoes Like Pottery</Link></li>
+            </ul>
+
+            <ul className="second-nav-list-mobile">
+              <li className="second-nav-item-mobile"><Link onClick={handleNavClick} to="/">About us</Link></li>
+              <li className="second-nav-item-mobile"><Link onClick={handleNavClick} to="/sanders">Contact</Link></li>
+            </ul>
+          </nav>
+        )}
       </header>
 
       <main className="main-mobile">
-        
         <Outlet />
-        
       </main>
-      {/* <footer>© 2024 Swipe Agency</footer> */}
     </>
   );
 }
