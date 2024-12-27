d77a841fffa25b4708468abd4aa4f2f29e471cd4
inesuribeb <inesuribeb@gmail.com>
2024-12-27 11:35:50 +0100

    moonstar 810s best sellers

--- src/pages/moonstar_810s/Moonstar_810sPhone.jsx
+++ src/pages/moonstar_810s/Moonstar_810sPhone.jsx
@@ -3,6 +3,7 @@ import Carrusel from '../../components/phone/carrusel/Carrusel'
 import History from '../../components/phone/textPhone/History';
 import Origin from '../../components/phone/textPhone/Origin';
 import Socials from '../../components/phone/textPhone/Socials';
+import BestSellers from '../../components/phone/bestSellers/BestSellers';
 
 import './Moonstar_810sPhone.css';
 
@@ -23,6 +24,24 @@ function Moonstar_810sPhone() {
             }
         ]
     };
+
+    const moonstar810sBs = {
+        images: [
+            {
+                url: 'https://i.ibb.co/P19FfrX/unive.png',
+                description: 'Unive Black'
+            },
+            {
+                url: 'https://i.ibb.co/3Rg9GVs/STUDEN-10.jpg',
+                description: 'Studen Black'
+            },
+            {
+                url: 'https://i.ibb.co/6cQ4JzD/kitche-black-2.jpg" alt="kitche-black-2',
+                description: 'Kitche Black'
+            }
+        ]
+    };
+
     return (
         <div>
             <Logo url="https://and-flow.jp/wp-content/uploads/2024/08/13196_sub1.png" />
@@ -32,6 +51,9 @@ function Moonstar_810sPhone() {
             <History brandText="Founded in 1873, Moonstar is a heritage Japanese footwear brand rooted in the traditions of Kurume, a city renowned for its fine craftsmanship. " />
             <History brandText="The Moonstar 810s line represents the perfect fusion of the brand’s century-old expertise and modern, functional design. Known for their vulcanized rubber soles and meticulous attention to detail, Moonstar 810 shoes are versatile, durable, and stylish. " />
             <History brandText="Every pair is made with precision in the Kurume factory, ensuring superior quality and comfort. Moonstar continues to innovate while honoring its rich legacy, making it a favorite among footwear enthusiasts worldwide." />
+            <div className="moonstar810sBs">
+                <BestSellers brandBsContent={moonstar810sBs} />
+            </div>
             <Socials
                 instagram="https://www.instagram.com/moonstar_810s/"
                 facebook="https://www.facebook.com/profile.php?id=61556541003931"
